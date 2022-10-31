8ad3c01ef7299fb7b35836e8a843957c1ea49897
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-11-01 02:03:33 +0400

    security fix and checks

--- src/contracts/vintage_marketplace_contract.py
+++ src/contracts/vintage_marketplace_contract.py
@@ -5,17 +5,16 @@ class Watch:
     class Variables:
         name = Bytes("NAME")
         image = Bytes("IMAGE")
-        description = Bytes("DESCRIPTION") 
+        description = Bytes("DESCRIPTION")
         price = Bytes("PRICE")
         owner = Bytes("OWNER")
-        
+        sale = Bytes("SALE")
 
     class AppMethods:
         buy = Bytes("buy")
         changeimage = Bytes("changeimage")
         changedescription = Bytes("changedescription")
-       
-
+        toggleSale = Bytes("toggleSale")
 
     def application_creation(self):
         return Seq([
@@ -28,7 +27,7 @@ class Watch:
                     Len(Txn.application_args[2]) > Int(0),
                     Btoi(Txn.application_args[3]) > Int(0),
                 )
-            ), 
+            ),
 
             # Store the transaction arguments into the applications's global's state
             App.globalPut(self.Variables.name, Txn.application_args[0]),
@@ -36,65 +35,70 @@ class Watch:
             App.globalPut(self.Variables.description, Txn.application_args[2]),
             App.globalPut(self.Variables.price, Btoi(Txn.application_args[3])),
             App.globalPut(self.Variables.owner, Txn.sender()),
+            App.globalPut(self.Variables.sale, Int(1)),
 
             Approve(),
         ])
 
     def buy(self):
-            count = Txn.application_args[1]
-            valid_number_of_transactions = Global.group_size() == Int(2)
+        valid_number_of_transactions = Global.group_size() == Int(2)
 
-            valid_payment_to_seller = And(
-                Gtxn[1].type_enum() == TxnType.Payment,
-                Gtxn[1].receiver() == App.globalGet(self.Variables.owner),
-                Gtxn[1].amount() == App.globalGet(self.Variables.price),
-                Gtxn[1].sender() == Gtxn[0].sender(),
-            )
+        valid_payment_to_seller = And(
+            Gtxn[1].type_enum() == TxnType.Payment,
+            Gtxn[1].receiver() == App.globalGet(self.Variables.owner),
+            Gtxn[1].amount() == App.globalGet(self.Variables.price),
+            Gtxn[1].sender() == Gtxn[0].sender(),
+        )
 
-            can_buy = And(valid_number_of_transactions,
-                        valid_payment_to_seller)
+        can_buy = And(
+            App.globalGet(self.Variables.sale) == Int(1),
+            valid_number_of_transactions,
+            valid_payment_to_seller)
 
-            update_state = Seq([
-                App.globalPut(self.Variables.owner, Txn.sender()),
-                Approve()
-            ])
+        update_state = Seq([
+            App.globalPut(self.Variables.sale, Int(0)),
+            App.globalPut(self.Variables.owner, Txn.sender()),
+            Approve()
+        ])
 
-            return If(can_buy).Then(update_state).Else(Reject())
-
-    
+        return If(can_buy).Then(update_state).Else(Reject())
 
     def changedescription(self):
         Assert(
             And(
-                    Global.group_size() == Int(1),
-                   
-                    Txn.sender() == App.globalGet(self.Variables.owner),
-                  
-                    Txn.application_args.length() == Int(2),
+                Txn.sender() == App.globalGet(self.Variables.owner),
+                Len(Txn.application_args[1]) > Int(0),
+                Txn.application_args.length() == Int(2),
             ),
         ),
         return Seq([
-            App.globalPut(self.Variables.description, Txn.application_args[2]),
+            App.globalPut(self.Variables.description, Txn.application_args[1]),
+            Approve()
+        ])
+
+    def toggleSale(self):
+        return Seq([
+            Assert(Txn.sender() == App.globalGet(self.Variables.owner)),
+            If(App.globalGet(self.Variables.sale) == Int(0)).Then(App.globalPut(
+                self.Variables.sale, Int(1))).Else(App.globalPut(self.Variables.sale, Int(0))),
             Approve()
         ])
 
     def changeimage(self):
         Assert(
             And(
-                    Global.group_size() == Int(1),
-                  
-                    Txn.sender() == App.globalGet(self.Variables.owner),
-                   
-                    Txn.application_args.length() == Int(2),
+                Txn.sender() == App.globalGet(self.Variables.owner),
+                Len(Txn.application_args[1]) > Int(0),
+                Txn.application_args.length() == Int(2),
             ),
         ),
         return Seq([
-            App.globalPut(self.Variables.image, Txn.application_args[2]),
+            App.globalPut(self.Variables.image, Txn.application_args[1]),
             Approve()
         ])
 
-
     # To delete a picture.
+
     def application_deletion(self):
         return Return(Txn.sender() == App.globalGet(self.Variables.owner),)
 
@@ -108,10 +112,12 @@ class Watch:
             [Txn.on_completion() == OnComplete.DeleteApplication,
              self.application_deletion()],
             # if the first argument of the transaction matches the AppMethods.buy value, the buy() method is called.
-            
+
             [Txn.application_args[0] == self.AppMethods.buy, self.buy()],
             [Txn.application_args[0] == self.AppMethods.changeimage, self.changeimage()],
-            [Txn.application_args[0] == self.AppMethods.changedescription, self.changedescription()],
+            [Txn.application_args[0] == self.AppMethods.toggleSale, self.toggleSale()],
+            [Txn.application_args[0] == self.AppMethods.changedescription,
+                self.changedescription()],
         )
 
     # The approval program is responsible for processing all application calls to the contract.
